ac2e0c32bc16ddfe9d6cbe478fda76c5e7a69a69
rayshoo <fire@dragonz.dev>
2021-07-18 02:57:19 +0900

    experimental dnf fastestmirror

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -7,8 +7,31 @@ OS_NAME=$(awk -F= '/^NAME/{print $2}' /etc/os-release | grep -o "\w*"| head -n 1
 
 case "${OS_NAME}" in
   "CentOS")
-    sudo yum install -y epel-release
-    sudo yum install -y ansible sshpass
+    version=$(grep '^VERSION_ID=' /etc/os-release | sed s'/VERSION_ID=//')
+
+    if [ $version == "\"8\"" ] ; then
+      options=( "fastestmirror=1" "max_parallel_downloads=8" )
+
+      for option in ${options[@]}; do
+      IFS='=' read -ra arr <<< $option
+        {
+          result=$(grep ${arr[0]} /etc/dnf.conf)
+        }||
+        {
+          result=""
+        }
+        if [ -z $result ] ; then
+          cat << EOF >> /etc/dnf.conf
+$option
+EOF
+        fi
+      done
+      sudo dnf install -y epel-release
+      sudo dnf install -y ansible sshpass
+    else
+      sudo yum install -y epel-release
+      sudo yum install -y ansible sshpass
+    fi
   ;;
   "Ubuntu")
     sudo apt-add-repository --yes --update ppa:ansible/ansible
